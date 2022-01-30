02a2c5dc6e42a596131d83a5606297889c1b5eb9
beanc16 <bean.christopher@outlook.com>
2022-01-30 13:33:12 -0500

    Add MongoResults

---
+++ MongoResults.js
@@ -0,0 +1,40 @@
+class MongoResults
+{
+    constructor({
+        results = null,
+        error = null,
+        statusCode,
+    })
+    {
+        this.results = results;
+        this.error = error;
+        
+        if (statusCode != null)
+        {
+            this.statusCode = statusCode;
+        }
+        // Assume call succeeded if no error is given
+        else if (error === null)
+        {
+            this.statusCode = 200;
+        }
+    }
+    
+    
+    
+    wasSuccessful()
+    {
+        if (this.results !== null || this.statusCode === 200)
+        {
+            return true;
+        }
+        
+        return false;
+    }
+}
+
+
+
+
+
+module.exports = MongoResults;
