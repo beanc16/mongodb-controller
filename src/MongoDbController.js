74b7ba134cf41aa67ea7779746126b348059dacf
beanc16 <bean.christopher@outlook.com>
2022-01-30 15:44:33 -0500

    Fix linting errors with controller

--- src/MongoDbController.js
+++ src/MongoDbController.js
@@ -25,84 +25,88 @@ class MongoDbController
 
     static async getAll(findParams = this.findParams)
     {
-        return new Promise(async (resolve, reject) =>
+        return new Promise((resolve, reject) =>
         {
-            await MongoDbControllerHelpers.validateStaticVariables({
+            MongoDbControllerHelpers.validateStaticVariables({
                 collectionName: this.collectionName,
                 Model: this.Model,
                 controllerName: this.name,
             })
+            .then(function ()
+            {
+                console.info("Querying resources from database...");
+    
+                MongoDbControllerHelpers.queryResources({
+                    connection: this._connection,
+                    findParams,
+                    collectionName: this.collectionName,
+                    sortOptions: this.sortOptions,
+                    Model: this.Model,
+                })
+                .then(function (mongoResults)
+                {
+                    console.info("Successfully queried resources from database.");
+                    resolve(mongoResults);
+                })
+                .catch(function (errResults)
+                {
+                    console.error("Failed to query resources from database:", errResults);
+                    reject(errResults);
+                });
+            })
             .catch(function (errors)
             {
                 reject(errors);
-            });
-
-            console.info("Querying resources from database...");
-
-            MongoDbControllerHelpers.queryResources({
-                connection: this._connection,
-                findParams,
-                collectionName: this.collectionName,
-                sortOptions: this.sortOptions,
-                Model: this.Model,
-            })
-            .then(function (mongoResults)
-            {
-                console.info("Successfully queried resources from database.");
-                resolve(mongoResults);
-            })
-            .catch(function (errResults)
-            {
-                console.error("Failed to query resources from database:", errResults);
-                reject(errResults);
             });
         });
     }
 
     static async getMostRecent(findParams = this.findParams)
     {
-        return new Promise(async (resolve, reject) =>
+        return new Promise((resolve, reject) =>
         {
-            await MongoDbControllerHelpers.validateStaticVariables({
+            MongoDbControllerHelpers.validateStaticVariables({
                 collectionName: this.collectionName,
                 Model: this.Model,
                 controllerName: this.name,
             })
+            .then(function ()
+            {
+                MongoDbControllerHelpers.queryResource({
+                    connection: this._connection,
+                    findParams,
+                    collectionName: this.collectionName,
+                    Model: this.Model,
+                })
+                .then((mongoResults) =>
+                {
+                    if (mongoResults && mongoResults.results)
+                    {
+                        console.info("Successfully queried resources from database.");
+                        resolve(mongoResults.results);
+                    }
+    
+                    else
+                    {
+                        console.error(
+                            "Successfully queried resources from " + 
+                            "database, but an unknown error " + 
+                            "occurred while parsing the results."
+                        );
+    
+                        reject(`An unknown error occurred in ` + 
+                               `${this.name}.getMostRecent()`);
+                    }
+                })
+                .catch(function (errResults)
+                {
+                    console.error("Failed to query resources from database:", errResults);
+                    resolve(errResults);
+                });
+            })
             .catch(function (errors)
             {
                 reject(errors);
-            });
-
-            MongoDbControllerHelpers.queryResource({
-                connection: this._connection,
-                findParams,
-                collectionName: this.collectionName,
-                Model: this.Model,
-            })
-            .then((mongoResults) =>
-            {
-                if (mongoResults && mongoResults.results)
-                {
-                    console.info("Successfully queried resources from database.");
-                    resolve(mongoResults.results);
-                }
-
-                else
-                {
-                    console.error(
-                        "Successfully queried resources from " + 
-                        "database, but an unknown error " + 
-                        "occurred while parsing the results."
-                    );
-
-                    reject(`An unknown error occurred in ` + 
-                           `${this.name}.getMostRecent()`);
-                }
-            })
-            .catch(function (errResults)
-            {
-                console.error("Failed to query resources from database:", errResults);
-                resolve(errResults);
             });
         });
     }
@@ -115,35 +119,37 @@ class MongoDbController
 
     static async insertOne(obj)
     {
-        return new Promise(async (resolve, reject) =>
+        return new Promise((resolve, reject) =>
         {
-            await MongoDbControllerHelpers.validateStaticVariables({
+            MongoDbControllerHelpers.validateStaticVariables({
                 collectionName: this.collectionName,
                 Model: this.Model,
                 controllerName: this.name,
             })
+            .then(function ()
+            {
+                console.info(`Inserting one ${this.Model.name} into database...`);
+    
+                MongoDbControllerHelpers.insertOne({
+                    connection: this._connection,
+                    obj,
+                    collectionName: this.collectionName,
+                    Model: this.Model,
+                })
+                .then((model) =>
+                {
+                    console.info(`Successfully inserted one ${this.Model.name} into database.`);
+                    resolve(model);
+                })
+                .catch((errResults) =>
+                {
+                    console.error(`Failed to insert one ${this.Model.name} into database:`, errResults);
+                    reject(errResults);
+                });
+            })
             .catch(function (errors)
             {
                 reject(errors);
-            });
-
-            console.info(`Inserting one ${this.Model.name} into database...`);
-
-            MongoDbControllerHelpers.insertOne({
-                connection: this._connection,
-                obj,
-                collectionName: this.collectionName,
-                Model: this.Model,
-            })
-            .then((model) =>
-            {
-                console.info(`Successfully inserted one ${this.Model.name} into database.`);
-                resolve(model);
-            })
-            .catch((errResults) =>
-            {
-                console.error(`Failed to insert one ${this.Model.name} into database:`, errResults);
-                reject(errResults);
             });
         });
     }
@@ -156,36 +162,38 @@ class MongoDbController
 
     static async findOneAndUpdate(findParams = this.findParams, obj)
     {
-        return new Promise(async (resolve, reject) =>
+        return new Promise((resolve, reject) =>
         {
-            await MongoDbControllerHelpers.validateStaticVariables({
+            MongoDbControllerHelpers.validateStaticVariables({
                 collectionName: this.collectionName,
                 Model: this.Model,
                 controllerName: this.name,
             })
+            .then(function ()
+            {
+                console.info(`Updating one ${this.Model.name} in database...`);
+    
+                MongoDbControllerHelpers.findOneAndUpdate({
+                    connection: this._connection,
+                    findParams,
+                    obj,
+                    collectionName: this.collectionName,
+                    Model: this.Model,
+                })
+                .then((model) =>
+                {
+                    console.info(`Successfully updated one ${this.Model.name} in database.`);
+                    resolve(model);
+                })
+                .catch((errResults) =>
+                {
+                    console.error(`Failed to update one ${this.Model.name} in database:`, errResults);
+                    reject(errResults);
+                });
+            })
             .catch(function (errors)
             {
                 reject(errors);
-            });
-
-            console.info(`Updating one ${this.Model.name} in database...`);
-
-            MongoDbControllerHelpers.findOneAndUpdate({
-                connection: this._connection,
-                findParams,
-                obj,
-                collectionName: this.collectionName,
-                Model: this.Model,
-            })
-            .then((model) =>
-            {
-                console.info(`Successfully updated one ${this.Model.name} in database.`);
-                resolve(model);
-            })
-            .catch((errResults) =>
-            {
-                console.error(`Failed to update one ${this.Model.name} in database:`, errResults);
-                reject(errResults);
             });
         });
     }
@@ -198,35 +206,37 @@ class MongoDbController
 
     static async findOneAndDelete(findParams)
     {
-        return new Promise(async (resolve, reject) =>
+        return new Promise((resolve, reject) =>
         {
-            await MongoDbControllerHelpers.validateStaticVariables({
+            MongoDbControllerHelpers.validateStaticVariables({
                 collectionName: this.collectionName,
                 Model: this.Model,
                 controllerName: this.name,
             })
+            .then(function ()
+            {
+                console.info(`Deleting one ${this.Model.name} from database...`);
+    
+                MongoDbControllerHelpers.findOneAndDelete({
+                    connection: this._connection,
+                    findParams,
+                    collectionName: this.collectionName,
+                    Model: this.Model,
+                })
+                .then((model) =>
+                {
+                    console.info(`Successfully deleted one ${this.Model.name} from database.`);
+                    resolve(model);
+                })
+                .catch((errResults) =>
+                {
+                    console.error(`Failed to delete one ${this.Model.name} from database:`, errResults);
+                    reject(errResults);
+                });
+            })
             .catch(function (errors)
             {
                 reject(errors);
-            });
-
-            console.info(`Deleting one ${this.Model.name} from database...`);
-
-            MongoDbControllerHelpers.findOneAndDelete({
-                connection: this._connection,
-                findParams,
-                collectionName: this.collectionName,
-                Model: this.Model,
-            })
-            .then((model) =>
-            {
-                console.info(`Successfully deleted one ${this.Model.name} from database.`);
-                resolve(model);
-            })
-            .catch((errResults) =>
-            {
-                console.error(`Failed to delete one ${this.Model.name} from database:`, errResults);
-                reject(errResults);
             });
         });
     }
